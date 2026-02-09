d188f92e80692aa5d6137ba6b3af84b324608032
alfinafifan1 <agusalfin1@gmail.com>
2026-02-09 16:00:05 +0700

    feat: create expedition schema & generate migration

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,14 @@
 import { Router } from 'express';
 import {authRoutes} from '@modules/auth/auth.routes';
 import {userRoutes} from '@modules/user/user.routes';
+import { expeditionRoutes } from '../modules/expedition/user.routes';
 
 const router = Router();
 
 // Mount routes
 router.use('/auth', authRoutes);
 router.use('/users', userRoutes);
+router.use('/expedition', expeditionRoutes);
 
 // API info endpoint
 router.get('/', (_req, res) => {
