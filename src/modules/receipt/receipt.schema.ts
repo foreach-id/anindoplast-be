c5bb2245d929eafd9ab7f685126c28fc461a8cd4
alfinafifan1 <agusalfin1@gmail.com>
2026-02-11 23:03:00 +0700

    refactor: adjust receipt module so that support multiple payload

--- src/modules/receipt/receipt.schema.ts
+++ src/modules/receipt/receipt.schema.ts
@@ -3,7 +3,7 @@ import { z } from 'zod';
 export const receiptSchemas = {
   generateReceipt: z.object({
     body: z.object({
-      deliveryNumber: z.string().min(1, 'Delivery number must be at least 1 character'),
+      deliveryNumber: z.array(z.string().min(1, 'Delivery number must be at least 1 character')).min(1, 'At least one delivery number is required'),
     }),
   }),
 };
