72acc39dab7634504d2476e55a089b43bdabe343
alfinafifan1 <agusalfin1@gmail.com>
2026-02-11 23:37:44 +0700

    fix: error build cause wrong import at index user module

--- src/modules/user/index.ts
+++ src/modules/user/index.ts
@@ -1,5 +1,5 @@
-export { default as UserController } from './user.controller';
-export { default as UserService } from './user.service';
-export { default as userRoutes } from './user.routes';
+export { UserController } from './user.controller';
+export { UserService } from './user.service';
+export { userRoutes } from './user.routes';
 export { userSchemas } from './user.schema';
 export * from './user.types';
