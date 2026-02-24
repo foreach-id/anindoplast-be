2db120f5d8dbad785cbe30a642b6cf3917e4b136
alfinafifan1 <agusalfin1@gmail.com>
2026-02-24 21:19:50 +0700

    feat: create product category module

---
+++ src/modules/productCategory/index.ts
@@ -0,0 +1,4 @@
+export { ProductCategoryController } from './productCategory.controller';
+export { ProductCategoryService } from './productCategory.service';
+export { productCategoryRoutes } from './productCategory.routes';
+export * from './productCategory.types';
